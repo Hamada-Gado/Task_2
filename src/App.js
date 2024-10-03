1693b566cf192e84bdd3cd71ed931645cd5e0135
Ahmed Gado <ahmedgado0100@gmail.com>
2024-10-03 09:48:27 +0300

    finish task 2

--- src/App.js
+++ src/App.js
@@ -1,42 +1,47 @@
 // External variables
 const express = require("express");
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 require("dotenv").config();
 //Make sure to add your MongoDB URI in the .env file as MONGO_URI="your mongodb uri"
 //Check db connection links in README file
 const MongoURI = process.env.MONGO_URI;
-const {createUser, getUsers, createBlog, filterBlog, editBlog}= require('./Routes/userController')
-
+const {
+  createUser,
+  getUsers,
+  createBlog,
+  filterBlog,
+  editBlog,
+} = require("./Routes/userController");
 
 //App variables
 const app = express();
 const port = process.env.PORT || "8000";
-const user = require('./Models/User');
+const user = require("./Models/User");
 // #Importing the userController
-
 
 // configurations
 // Mongo DB
-mongoose.connect(MongoURI)
-.then(()=>{
-  console.log("MongoDB is now connected!")
-// Starting server
- app.listen(port, () => {
-    console.log(`Listening to requests on http://localhost:${port}`);
+mongoose
+  .connect(MongoURI)
+  .then(() => {
+    console.log("MongoDB is now connected!");
+    // Starting server
+    app.listen(port, () => {
+      console.log(`Listening to requests on http://localhost:${port}`);
+    });
   })
-})
-.catch(err => console.log(err));
+  .catch((err) => console.log(err));
 app.get("/home", (req, res) => {
-    res.status(200).send("You have everything installed!");
-  });
+  res.status(200).send("You have everything installed!");
+});
 
-app.use(express.json())
-app.post("/addUser",createUser);
-app.get('/users', getUsers);
+app.use(express.json());
+app.post("/addUser", createUser);
+app.get("/users", getUsers);
 
-app.post("/addBlog",createBlog);
-app.get('/filter',filterBlog);
-// TODO : add the editBlog route here
+app.post("/addBlog", createBlog);
+app.get("/filter", filterBlog);
+app.get("/editBlog/:id", editBlog);
 
 /*
                                                     End of your code
